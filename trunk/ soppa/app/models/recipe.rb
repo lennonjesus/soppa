9b07483e63c7ae726ebf5ef04240698aca61cdae
lennon.jesus <lennon.jesus@0d86d96c-a3c6-11dd-a887-7ffe1a420f8d>
2009-10-07 19:02:47 +0000

    de novo...

--- trunk/ soppa/app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-class Recipe < ActiveRecord::Base
-
-  belongs_to :recipe_type
-  has_many :recipe_items
-
-end
--- trunk/ soppa/db/migrate/20090824232046_add_city_to_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddCityToProfile < ActiveRecord::Migration
-  def self.up
-    add_column :profiles, :city_id, :integer
-  end
-
-  def self.down
-    remove_column :profiles, :city_id
-  end
-end
