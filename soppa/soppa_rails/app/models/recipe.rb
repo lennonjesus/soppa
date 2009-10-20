6e071eedbc1e19fb3328c1283a8236c2edc76f2d
lennon.jesus <lennon.jesus@0d86d96c-a3c6-11dd-a887-7ffe1a420f8d>
2009-10-20 00:20:37 +0000

    novas implementacoes e correcoes

--- soppa/soppa_rails/app/models/recipe.rb
+++ soppa/soppa_rails/app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :recipe_type
   has_many :recipe_items
   has_many :comments
+  belongs_to :user
 
   validates_presence_of :name, :description, :prepare_mode
 
--- soppa/soppa_rails/app/models/user.rb
+++ soppa/soppa_rails/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_one :profile
   has_many :comments
+  has_many :recipes
 
   # Virtual attribute for the unencrypted password
   attr_accessor :password
